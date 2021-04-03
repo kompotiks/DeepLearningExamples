88f7ab735bb90029693afb9b5f293245158e6531
kompotiks <lifami40@gmail.com>
2021-04-03 15:09:29 +0300

    New mels

--- PyTorch/SpeechSynthesis/Tacotron2/create_filelists.py
+++ PyTorch/SpeechSynthesis/Tacotron2/create_filelists.py
@@ -6,7 +6,7 @@ def create_markup(path_data='mozilla-1', path_markup='filelists'):
     with open(os.path.join(path_data, 'metadata.csv'), 'r', encoding='utf-8') as f:
         data = f.read().split('\n')[:-1]
 
-    data = [os.path.join(path_data, 'wavs', sample) for sample in data]
+    data = [f'{path_data}/wavs/{sample}' for sample in data]
     train, test = train_test_split(data, test_size=0.2, random_state=42)
     test, valid = train_test_split(test, test_size=0.3, random_state=42)
 
@@ -19,7 +19,7 @@ def create_markup(path_data='mozilla-1', path_markup='filelists'):
     with open(os.path.join(path_markup, 'ljs_audio_text_val_filelist.txt'), 'w', encoding='utf-8') as f:
         [f.write(sample + '\n') for sample in valid]
 
-    data = [os.path.join(path_data, 'mels', sample.replace('.wav', '.pt')) for sample in data]
+    data = [f'{path_data}/wavs/{sample.replace(".wav", ".pt")}' for sample in data]
     train, test = train_test_split(data, test_size=0.2, random_state=42)
     test, valid = train_test_split(test, test_size=0.3, random_state=42)
 
