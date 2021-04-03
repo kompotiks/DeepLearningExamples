947eb9048eb2632346d442986a528760584e4d11
kompotiks <lifami40@gmail.com>
2021-04-03 15:39:59 +0300

    Fix lists file

--- PyTorch/SpeechSynthesis/Tacotron2/create_filelists.py
+++ PyTorch/SpeechSynthesis/Tacotron2/create_filelists.py
@@ -4,9 +4,9 @@ from sklearn.model_selection import train_test_split
 
 def create_markup(path_data='mozilla-1', path_markup='filelists'):
     with open(os.path.join(path_data, 'metadata.csv'), 'r', encoding='utf-8') as f:
-        data = f.read().split('\n')[:-1]
+        data_raw = f.read().split('\n')[:-1]
 
-    data = [f'{path_data}/wavs/{sample}' for sample in data]
+    data = [f'{path_data}/wavs/{sample}' for sample in data_raw]
     train, test = train_test_split(data, test_size=0.2, random_state=42)
     test, valid = train_test_split(test, test_size=0.3, random_state=42)
 
@@ -19,7 +19,7 @@ def create_markup(path_data='mozilla-1', path_markup='filelists'):
     with open(os.path.join(path_markup, 'ljs_audio_text_val_filelist.txt'), 'w', encoding='utf-8') as f:
         [f.write(sample + '\n') for sample in valid]
 
-    data = [f'{path_data}/wavs/{sample.replace(".wav", ".pt")}' for sample in data]
+    data = [f'{path_data}/mels/{sample.replace(".wav", ".pt")}' for sample in data_raw]
     train, test = train_test_split(data, test_size=0.2, random_state=42)
     test, valid = train_test_split(test, test_size=0.3, random_state=42)
 
